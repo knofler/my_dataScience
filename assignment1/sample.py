16f3b257b72d292ea1a356b9f45ab369755c7de2
Rumman Ahmed <ahmed.rumman@gmail.com>
2015-02-18 02:00:16 +1100

    map reduce ongoing

--- assignment1/sample.py
+++ assignment1/sample.py
@@ -7,12 +7,16 @@ with open('AFINN-111.txt') as f:
     reader = csv.reader(f, delimiter='\t')
     for row in reader:
         scores[row[0].strip()] = int(row[1].strip()) 
+   
 
 
 with open('ass1_tweet.txt') as f:
     for line in f:
+       # print line
         tweet = json.loads(line)
+        # print tweet
         text = tweet.get('text','').encode('utf-8')
+        # print text
         if text:
             total_sentiment = sum(scores.get(word,0) for word in text.split())
             print("{}: {}".format(text,scores))
---
+++ assignment3/join.py
@@ -0,0 +1,39 @@
+import MapReduce
+import sys
+
+"""
+Word Count Example in the Simple Python MapReduce Framework
+"""
+
+mr = MapReduce.MapReduce()
+
+# =============================
+# Do not modify above this line
+
+def mapper(record):
+    # key: document identifier
+    # value: document contents
+    key = record
+    id = record[1]
+    # words = value.split()
+    # print "key", key
+    # print "id",id
+    for w in id:
+
+      print w
+      # mr.emit_intermediate(w, key)
+
+def reducer(key, list_of_values):
+    # key: word
+    # value: list of occurrence counts
+    total = []
+    for v in list_of_values:
+      total.append(v)
+      # print total
+    mr.emit((total))
+
+# Do not modify below this line
+# =============================
+if __name__ == '__main__':
+  inputdata = open(sys.argv[1])
+  mr.execute(inputdata, mapper, reducer)
